0d2e4538e67e5b0e8fb06b35c3c34f398817ef5e
Jack Hilscher <Jack@Jacks-MacBook-Air.local>
2019-03-12 16:05:46 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
 # code here!
+require 'pry'
+class School
+
+  attr_reader :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    if @roster[grade].class != Array
+      @roster[grade] = []
+    end
+    @roster[grade] << name
+  end
+
+  def grade(number)
+    @roster[number]
+  end
+
+  def sort
+    @roster.map {|grade, student| student.sort!}
+    @roster
+  end
+  
+end
