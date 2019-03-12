828ef2311b3ebbb1d1ee1b062318467d51c736ab
Flatiron School <flatironschool@Flatirons-MacBook-Air.local>
2019-03-12 17:02:44 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+class School
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, year)
+    @roster[year] ? @roster[year] << name : @roster[year] = [name]
+  end
+
+  def grade(year)
+    @roster[year]
+  end
+
+  def sort
+    result = {}
+    @roster.each { |k, v| result[k] = v.sort }
+    result
+
+  end
+
+
+end
